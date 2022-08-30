fcc416fa7678a8514faf73f22620bb8d14bdc32c
OliYeet <61905765+OliYeet@users.noreply.github.com>
2022-08-30 21:17:18 +0200

    - organize files into folder - add content mode - set content model as environment object - rename content view

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Oliwier Woznicki on 30.08.22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
