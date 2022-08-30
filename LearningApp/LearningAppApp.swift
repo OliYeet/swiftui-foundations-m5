9d4ae0e393f81750e7be974db85ec19828d9a692
OliYeet <61905765+OliYeet@users.noreply.github.com>
2022-08-30 21:11:12 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Oliwier Woznicki on 30.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
